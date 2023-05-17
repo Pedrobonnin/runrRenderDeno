a5ad5d05cf97e196a7f506d47e0dbf8dd08b8db8
Pedrobonnin <80923995+Pedrobonnin@users.noreply.github.com>
2023-05-17 19:58:17 -0300

    Add files via upload
    actualizo deno.ts con html

--- deno.ts
+++ deno.ts
@@ -1,19 +1,42 @@
+
+import { serve } from "https://deno.land/std@0.187.0/http/server.ts";
+
+
+
 import { send } from 'https://deno.land/std/http/mod.ts';
 
- 
+let statusMessage = "Hola Mundo"
+
 async function makeRequest() {
-  
   const now = new Date();
-  console.log(`[${now.toLocaleString()}] Making request...`);
+  const message = `[${now.toLocaleString()}] Making request...\n`;
+  console.log(message);
+
+
   const res = await fetch('https://pabbackend.onrender.com');
-  console.log(`[${now.toLocaleString()}] Status code: ${res.status}`);
+  statusMessage = `[${now.toLocaleString()}] Status code: ${res.status}\n`;
+  console.log(statusMessage);
+
+
   if (res.status === 200) {
-    console.log(`[${now.toLocaleString()}] Request successful!`);
+    const successMessage = `[${now.toLocaleString()}] Request successful!\n`;
+    console.log(successMessage);
+
+
     // Aquí puedes procesar la respuesta de la petición
   } else {
-    console.error(`[${now.toLocaleString()}] Error: ${res.statusText}`);
+    const errorMessage = `[${now.toLocaleString()}] Error: ${res.statusText}\n`;
+    console.error(errorMessage);
+
   }
 }
 
-setInterval(makeRequest, 14 * 60 * 1000); // 14 minutos en milisegundos
+setInterval(makeRequest, 0 * 60 * 1000); // 14 minutos en milisegundos
 
+
+serve((_req)=> {
+    return new Response(statusMessage, {
+      headers: { "content-type": "text/plain" },
+    });
+    
+  });
